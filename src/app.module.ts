514dd2986bf64bef95a920d42dbca935e2c1b17e
webcat12345 <tian.jang@yandex.com>
2020-02-05 09:02:08 -0800

    feat(graphql): initialize graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BookModule } from './book/book.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      typePaths: ['./**/*.graphql'],
+      installSubscriptionHandlers: true,
+    }),
+    BookModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
